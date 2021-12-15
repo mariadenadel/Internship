8e5dae18b2dc144604abdd04c9ab353784adc9b9
mariadenadel <maria.denadel@gmail.com>
2021-12-15 16:38:01 +0200

    add associations for models

--- rails-homework/app/models/patient.rb
+++ rails-homework/app/models/patient.rb
@@ -1,4 +1,6 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :physicians, through: :appointments
+  has_many :diagnosis_items
+  has_many :physicians, through: :diagnosis_items
 end
--- rails-homework/app/models/physician.rb
+++ rails-homework/app/models/physician.rb
@@ -1,4 +1,6 @@
 class Physician < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
+  has_many :diagnosis_items
+  has_many :patients, through: :diagnosis_items
 end
