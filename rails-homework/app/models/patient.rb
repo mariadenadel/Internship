6c64fbc61610643a0205ae5cc0580bece96be79c
mariadenadel <maria.denadel@gmail.com>
2021-12-15 17:54:28 +0200

    add valitations for Patient and Physician models

--- rails-homework/app/models/patient.rb
+++ rails-homework/app/models/patient.rb
@@ -3,4 +3,7 @@ class Patient < ApplicationRecord
   has_many :physicians, through: :appointments
   has_many :diagnosis_items
   has_many :physicians, through: :diagnosis_items
+
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "enter valid email" }
 end
--- rails-homework/app/models/physician.rb
+++ rails-homework/app/models/physician.rb
@@ -3,4 +3,7 @@ class Physician < ApplicationRecord
   has_many :patients, through: :appointments
   has_many :diagnosis_items
   has_many :patients, through: :diagnosis_items
+
+  validates :phone_number, presence: true, format: { with: /\A\+380[\d]{9}\z/, message: "enter valid UA phone number" }
+  validates :level, presence: true, inclusion: { in: %w(junior middle senior) }
 end
