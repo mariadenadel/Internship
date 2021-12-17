e12131927a964164ed8de5012f8f1056df95a2bd
mariadenadel <maria.denadel@gmail.com>
2021-12-17 17:26:58 +0200

    add edit, update, destroy, new, create actions for physicians

--- rails-homework/app/controllers/physicians_controller.rb
+++ rails-homework/app/controllers/physicians_controller.rb
@@ -6,4 +6,43 @@ class PhysiciansController < ApplicationController
   def show
     @physician = Physician.find(params[:id])
   end
+
+  def new
+    @physician = Physician.new
+  end
+
+  def create
+    @physician = Physician.new(physician_params)
+    if @physician.save
+      redirect_to @physician
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @physician = Physician.find(params[:id])
+  end
+
+  def update
+    @physician = Physician.find(params[:id])
+    if @physician.update(physician_params)
+      redirect_to @physician
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @physician = Physician.find(params[:id])
+    @physician.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def physician_params
+    params.require(:physician).permit(:first_name, :last_name, :phone_number, :level)
+  end
+
 end
