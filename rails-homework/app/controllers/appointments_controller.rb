17a2019cceec93fe2c3671f0e0e33d61a23cda0c
mariadenadel <maria.denadel@gmail.com>
2021-12-28 12:52:28 +0100

    create show, create, edit, new, update and destroy VC for appointments

--- rails-homework/app/controllers/appointments_controller.rb
+++ rails-homework/app/controllers/appointments_controller.rb
@@ -2,4 +2,46 @@ class AppointmentsController < ApplicationController
   def index
     @appointments = Appointment.all
   end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to @appointment
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(appointment_params)
+      redirect_to @appointment
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    redirect_to "/appointments"
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:appointment_date, :patient_id, :physician_id)
+  end
 end
